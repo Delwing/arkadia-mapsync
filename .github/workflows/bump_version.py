1d894e8f6926113a22884a16033e9985fed98bb8
Piotr Wilczynski <delwing@gmail.com>
2021-10-29 20:52:38 +0200

    bump version fix

--- .github/workflows/bump_version.py
+++ .github/workflows/bump_version.py
@@ -1,16 +1,17 @@
 import re
+import sys
 
 with open('map_sync.lua', 'r') as file:
     data = file.readlines()
 
-print(data[0])
-version = re.search("\D*(\d+)\.(\d+).*", data[0])
+versionString = len(sys.argv) > 1 and sys.argv[1] or data[3]
+version = re.search("\D*(\d+)\.(\d+).*", versionString)
 versionLine = version.group(0)
 major = version.group(1)
 minor = int(version.group(2)) + 1
 
-newVersionLine = re.sub("\"((?:\d+\.?)*)\"", '"{0}.{1}"'.format(major, minor), versionLine)
-data[0] = newVersionLine + "\n"
+newVersionLine = re.sub("\"((?:\d+\.?)*)\"", '"{0}.{1}"'.format(major, minor), data[3])
+data[3] = newVersionLine
 
 
 with open('map_sync.lua', 'w') as file:
