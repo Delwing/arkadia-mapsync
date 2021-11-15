35a3be16d61e999fbffa7992a30fd110f1c704f6
Piotr Wilczynski <delwing@gmail.com>
2021-11-15 15:24:51 +0100

    fix upload

--- map_sync/upload.lua
+++ map_sync/upload.lua
@@ -1,20 +1,18 @@
 function map_sync:show_upload_link()
     map_sync["api_response"] = nil
-    map_sync:download_state()
-    tempTimer(1, function()
-        map_sync:show_upload_msg()
+    map_sync:print_state(function(response)
+        map_sync:show_upload_msg(response)
     end)
 end
 
-function map_sync:show_upload_msg()
-    if not map_sync["api_response"] then
-        cecho("\n <orange> Map Sync: Problem z komunikacja z serwerem\n")
-        return
+function map_sync:show_upload_msg(response)
+   if response.version == map_sync.current_version then
+        cecho("\n <orange> Map Sync Upload\n\n  (1) Musisz miec uprawnienia do wrzucania map\n  (2) Pamietaj aby po rozpoczeciu locka pobrac aktualna wersje mapy z serwera.\n  (3) Twoja wersja: <green>" .. map_sync.current_version .. "<orange>, wersja na serwerze: <green>" .. response.version .. "<orange>\n  (4) Kliknij ")
+        echoLink("tutaj", [[ map_sync_open_upload_www() ]], "klik")
+        cecho(" <orange>aby wrzucic mape\n")
+    else
+        cecho("\n <orange> Map Sync - pobierz aktualna wersje mapy przed zalozeniem locka. Twoja wersja: <green>" .. map_sync.current_version .. "<orange>, wersja na serwerze: <green>" .. response.version .. "<reset>")
     end
-
-    cecho("\n <orange> Map Sync Upload\n\n  (1) Musisz miec uprawnienia do wrzucania map\n  (2) Pamietaj aby po rozpoczeciu locka pobrac aktualna wersje mapy z serwera.\n  (3) Twoja wersja: <green>" .. map_sync["current_version"] .. "<orange>, wersja na serwerze: <green>" .. map_sync["api_response"]["version"] .. "<orange>\n  (4) Kliknij ")
-    echoLink("tutaj", [[ map_sync_open_upload_www() ]], "klik")
-    cecho(" <orange>aby wrzucic mape\n")
 end
 
 function map_sync_open_upload_www()
